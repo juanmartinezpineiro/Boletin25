22d654fc170dee1a742ffa482a55d1ade3bb8f6c
jmartinezpineiro <jmartinezpineiro@nautilus16.danielcastelao.org>
2019-04-02 12:25:56 +0200

    Boletin terminado

---
+++ src/boletin25_1/Boletin25_1.java
@@ -0,0 +1,19 @@
+package boletin25_1;
+
+/**
+ *
+ * @author jmartinezpineiro
+ */
+public class Boletin25_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+
+
+
+    }
+
+}
